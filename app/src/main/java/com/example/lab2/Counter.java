34e1d2be261d3d7f53dc810ea8ba63f772a00dfd
Karolis0 <36989486+Karolis0@users.noreply.github.com>
2022-03-12 15:25:23 +0200

    Initial commit, L2 Taikomieji sprendimai - Counter

---
+++ app/src/main/java/com/example/lab2/Counter.java
@@ -0,0 +1,70 @@
+package com.example.lab2;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Counter {
+
+    public int Count(String _wordToCount, String _countType) {
+        if (Objects.equals(_countType,"Words"))
+        {
+            String[] parts = this.SplitString(_wordToCount);
+            return parts.length;
+        }
+        else if (Objects.equals(_countType,"Char"))
+        {
+            int charCount = 0;
+            String[] parts = this.SplitString(_wordToCount);
+            for (int i = 0; i < parts.length; i++)
+            {
+                charCount += parts[i].trim().length();
+            }
+            return charCount;
+        }
+
+        else if (Objects.equals(_countType,"Character and space"))
+        {
+            int charCount = 0;
+            for (int i = 0; i < _wordToCount.length(); i++)
+            {
+                charCount++;
+            }
+            return charCount;
+        }
+
+        else if (Objects.equals(_countType,"Letter"))
+        {
+            int charCount = 0;
+
+            for (int i = 0; i < _wordToCount.length(); i++)
+            {
+                if (Character.isLetter(_wordToCount.charAt(i))) {
+                    charCount++;
+                }
+            }
+            return charCount;
+        }
+
+        else if (Objects.equals(_countType,"Numbers"))
+        {
+            int charCount = 0;
+            for (int i = 0; i < _wordToCount.length(); i++)
+            {
+                if (Character.isDigit(_wordToCount.charAt(i))) {
+                    charCount++;
+                }
+            }
+            return charCount;
+        }
+        else
+        {
+            return _wordToCount.length();
+        }
+    }
+
+    public String[] SplitString(String _stringToSplit)
+    {
+        return _stringToSplit.split(" ");
+    }
+
+}
---
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvInput;
+    TextView tvResult;
+    Button btnCalculate;
+    Spinner spnSelection;
+    Counter counter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvInput = (TextView) findViewById(R.id.txtInput);
+        tvResult = (TextView) findViewById(R.id.txtResults);
+        btnCalculate = (Button) findViewById(R.id.btnCount);
+        spnSelection = (Spinner) findViewById(R.id.spnSelection);
+
+        counter = new Counter();
+
+        //Spinner init (3)
+        String[] arraySpinner = new String[] {
+                getString(R.string.Words),
+                getString(R.string.Characters),
+                getString(R.string.CharactersWSpace),
+                getString(R.string.Letters),
+                getString(R.string.Numbers)
+        };
+        Spinner s = (Spinner) findViewById(R.id.spnSelection);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+
+        // 1,
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String res1 = tvInput.getText().toString();
+                String res2 = spnSelection.getSelectedItem().toString();
+
+                int CountResult = counter.Count(tvInput.getText().toString(), spnSelection.getSelectedItem().toString());
+                if (CountResult > 0) {
+                    tvResult.setText(Integer.toString(CountResult));
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),getString(R.string.FalseInput),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
